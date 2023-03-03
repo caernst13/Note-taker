14992cbb1a2f42757a0fc908fad617b12d04867b
caernst13 <caleb_ernst@yahoo.com>
2023-03-03 16:26:49 -0500

    added basic notes routes for getiing and posting with minimal functionality

---
+++ routes/notes.js
@@ -0,0 +1,27 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) => {
+    console.info(`${req.method} request received`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+        readAndAppend(newNote, './db/db.json')
+    }
+})
+
+notes.delete()
+
+
+module.exports = notes;
